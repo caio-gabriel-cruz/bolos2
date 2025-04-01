fb2514cabb03de7b957c945e1c8f9c6d1bf8b986
caio-gabriel-cruz <killua37pt@gmail.com>
2025-04-01 20:11:41 -0300

    sim

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -1,32 +1,74 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+<html lang="pt-br">
+    <head>
+        <title>Super Gestão - Contato</title>
+        <meta charset="utf-8">
 
-    <title>Contato</title>
-</head>
-<body>
-    <main>
-        <section class='esquerdinha'>
-            <h1>video</h1>
-            <p><span class='bola'></span>sim</p>
-            <p><span class='bola'></span>nao</p>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/9h1l5Z0mH0w?si=wj9ariJXkyQeWy_t" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        </section>
-        <section class='direitinha'>
-            <h1>contato</h1>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum a explicabo alias ipsum quae</p>
-            <form action="">
-                <input type="text" placeholder='nome'>
-                <input type="text" placeholder='cpf'>
-                <input type="text" placeholder='email'>
-                <input type="text" placeholder='senha'>
-                <input type="text" placeholder='confirmar senha'>
-                <button type="submit">Enviar</button>
-            </form>
-        </section>
-    </main>
-</body>
+        <link rel="stylesheet" href="{{asset('css/contato.css')}}">
+    </head>
+
+    <body>
+        <div class="topo">
+
+            <div class="logo">
+                <img src="img/logo.png">
+            </div>
+
+            <div class="menu">
+                <ul>
+                    <li><a href="/">Principal</a></li>
+                    <li><a href="/sobre">Sobre Nós</a></li>
+                    <li><a href="/contato">Contato</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="conteudo-pagina">
+            <div class="titulo-pagina">
+                <h1>Entre em contato conosco</h1>
+            </div>
+
+            <div class="informacao-pagina">
+                <div class="contato-principal">
+                    <form>
+                        <input type="text" placeholder="Nome" class="borda-preta">
+                        <br>
+                        <input type="text" placeholder="Telefone" class="borda-preta">
+                        <br>
+                        <input type="text" placeholder="E-mail" class="borda-preta">
+                        <br>
+                        <select class="borda-preta">
+                            <option value="">Qual o motivo do contato?</option>
+                            <option value="">Dúvida</option>
+                            <option value="">Elogio</option>
+                            <option value="">Reclamação</option>
+                        </select>
+                        <br>
+                        <textarea class="borda-preta">Preencha aqui a sua mensagem</textarea>
+                        <br>
+                        <button type="submit" class="borda-preta">ENVIAR</button>
+                    </form>
+                </div>
+            </div>  
+        </div>
+
+        <div class="rodape">
+            <div class="redes-sociais">
+                <h2>Redes sociais</h2>
+                <img src="img/facebook.png">
+                <img src="img/linkedin.png">
+                <img src="img/youtube.png">
+            </div>
+            <div class="area-contato">
+                <h2>Contato</h2>
+                <span>(11) 3333-4444</span>
+                <br>
+                <span>supergestao@dominio.com.br</span>
+            </div>
+            <div class="localizacao">
+                <h2>Localização</h2>
+                <img src="img/mapa.png">
+            </div>
+        </div>
+    </body>
 </html>
--- resources/views/site/sobre.blade.php
+++ resources/views/site/sobre.blade.php
@@ -1,11 +1,55 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sobre</title>
-</head>
-<body>
-    <h1>Sobre</h1>
-</body>
+<html lang="pt-br">
+    <head>
+        <title>Super Gestão - Sobre Nós</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="{{asset('css/sobre.css')}}">
+    </head>
+
+    <body>
+        <div class="topo">
+
+            <div class="logo">
+                <img src="img/logo.png">
+            </div>
+
+            <div class="menu">
+                <ul>
+                    <li><a href="/">Principal</a></li>
+                    <li><a href="/sobre">Sobre Nós</a></li>
+                    <li><a href="/contato">Contato</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="conteudo-pagina">
+            <div class="titulo-pagina">
+                <h1>Olá, eu sou o Super Gestão</h1>
+            </div>
+
+            <div class="informacao-pagina">
+                <p>O Super Gestão é o sistema online de controle administrativo que pode transformar e potencializar os negócios da sua empresa.</p>
+                <p>Desenvolvido com a mais alta tecnologia para você cuidar do que é mais importante, seus negócios!</p>
+            </div>  
+        </div>
+
+        <div class="rodape">
+            <div class="redes-sociais">
+                <h2>Redes sociais</h2>
+                <img src="img/facebook.png">
+                <img src="img/linkedin.png">
+                <img src="img/youtube.png">
+            </div>
+            <div class="area-contato">
+                <h2>Contato</h2>
+                <span>(11) 3333-4444</span>
+                <br>
+                <span>supergestao@dominio.com.br</span>
+            </div>
+            <div class="localizacao">
+                <h2>Localização</h2>
+                <img src="img/mapa.png">
+            </div>
+        </div>
+    </body>
 </html>
