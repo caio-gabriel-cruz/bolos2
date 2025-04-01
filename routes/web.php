da04ac46697dd9ca3fa2316dc7d25bd5f8d6a7a6
caio-gabriel-cruz <killua37pt@gmail.com>
2025-03-31 22:14:38 -0300

    se quiser nao

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,25 @@ Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
 Route::get('/sobre', [\App\Http\Controllers\SobreNosController::class, 'sobre']);
 
+Route::prefix('/api')->group(function() {
+    Route::get('/produtos', function() {
+        return 'produtos';
+    });
+    
+    Route::get('/fornecedores', function() {
+        return 'fornecedores';
+    });
+    
+    Route::get('/clientes', function() {
+        return 'clientes';
+    });
+    
+});
+
 Route::get('/login', function() {
     return 'Login';
 });
 
-Route::get('/produtos', function() {
-    return 'produtos';
-});
-
-Route::get('/fornecedores', function() {
-    return 'fornecedores';
-});
 
 Route::fallback(function(){
     echo 'A rota acessada não existe. <a href="/">Clique aqui</a> para ir para a página inicial.';
